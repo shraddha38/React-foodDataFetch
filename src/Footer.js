532bffae44645cbc0aac1d7f30a947b7a8ff8623
shraddha38 <64234342+shraddha38@users.noreply.github.com>
2021-08-18 14:21:06 +0530

    Update Footer.js
    Refactor

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css'
 const Footer = () => (
   <footer className="footer">
-    <p>Urban Bistro! Nashik,Maharashtra.  Order Now! : 6267519419</p>
+    <p>Urban Bistro! Nashik,Maharashtra.  Order Now! : 567-3456-569</p>
   
   </footer>
 );
